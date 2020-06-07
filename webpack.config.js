d2100d190bda117cc8cebde26d41b4d0f83dbd4d
tmirun <tmirun@hotmail.com>
2020-06-07 23:17:17 +0200

    add test page

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './src'),
+        compress: true,
+        port: 9000,
+        index: './index.html',
+        watchContentBase: true,
+        hot: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
+    ],
+};
